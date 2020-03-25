cc8ae80d12cec5a9d0dd1bb59e29f5b527dd7764
Dustin Martens <dmartens.it@gmail.com>
2020-03-24 20:24:43 -0500

    Added alphabetized action/view with route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'customers/index'
+  root to: 'customers#index'
+  get '/customers/alphabetized'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
